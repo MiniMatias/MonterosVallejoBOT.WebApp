906925710716928ee1013b2c311d4bb1b73d5c1f
matias <matia@MATIPC>
2025-05-18 23:39:56 -0500

    se creo clase applicationDb y modelo chatresponse

---
+++ MonterosVallejoBOT.WebApp/Models/ChatResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations; 
+
+namespace MonterosVallejoBOT.WebApp.Models;
+
+public class ChatResponse
+{
+    [Key] 
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "La respuesta no puede estar vacía.")]
+    public string? Respuesta { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "El proveedor no puede estar vacío.")]
+    public string? Proveedor { get; set; } 
+
+    [Required(ErrorMessage = "El campo 'GuardadoPor' no puede estar vacío.")]
+    public string? GuardadoPor { get; set; }
+}

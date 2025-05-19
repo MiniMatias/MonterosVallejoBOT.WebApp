3e79933d765c14bb2610a153a814ed35ccca18f5
matias <matia@MATIPC>
2025-05-18 23:31:53 -0500

    Se modifico index.razor

--- MonterosVallejoBOT.WebApp/Program.cs
+++ MonterosVallejoBOT.WebApp/Program.cs
@@ -1,28 +1,56 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using MonterosVallejoBOT.WebApp.Data;
+using MonterosVallejoBOT.WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+
+builder.Services.AddScoped<GeminiApiService>();
+builder.Services.AddScoped<OpenAIApiService>(); 
+
+builder.Services.AddScoped<IChatBotService, ChatBotService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    try
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.EnsureCreated();
+            app.Logger.LogInformation("Base de datos 'monterosvallejobot_data.db' asegurada/creada.");
+        }
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "Ocurrió un error al asegurar/crear la base de datos.");
+    }
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();

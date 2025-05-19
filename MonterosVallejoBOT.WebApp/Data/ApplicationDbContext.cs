aea7b201c83b089c5facc36070c3b2ce8e7085e3
matias <matia@MATIPC>
2025-05-19 00:06:23 -0500

    Se realizaron correcciones de codigo y nugets

--- MonterosVallejoBOT.WebApp/Data/ApplicationDbContext.cs
+++ MonterosVallejoBOT.WebApp/Data/ApplicationDbContext.cs
@@ -10,20 +10,13 @@ public class ApplicationDbContext : DbContext
     {
     }
 
-    // DbSet para tu entidad. EF Core creará una tabla basada en esto.
     public DbSet<ChatResponse> ChatResponses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        // Aquí puedes añadir configuraciones adicionales para tus entidades usando Fluent API
-        // Por ejemplo, especificar el nombre de la tabla explícitamente:
+    
         modelBuilder.Entity<ChatResponse>().ToTable("HistorialChat");
-
-        // Ejemplo de configuración adicional si tuvieras más entidades o relaciones:
-        // modelBuilder.Entity<ChatResponse>()
-        //     .Property(p => p.Respuesta)
-        //     .HasMaxLength(2000); // Si quisieras limitar la longitud
     }
 }
